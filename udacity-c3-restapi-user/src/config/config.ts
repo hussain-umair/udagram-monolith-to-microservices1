1f6b93cb478a351c9aa323c1da92b8c9d65cdefe
MrLongBeard <umair.hussain96@gmail.com>
2020-05-22 21:25:29 +0500

    first commit

---
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -0,0 +1,32 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DB,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET
+    
+    // "username": "umair",
+    // "password": "umairhussain",
+    // "database": "cdrdb",
+    // "host": "cdr.c3elwqveng6j.us-east-1.rds.amazonaws.com",
+    // "dialect": "postgres",
+    // "aws_reigion": "us-east-1",
+    // "aws_profile": "default",
+    // "aws_media_bucket": "test-microservices-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  }
+
+}
